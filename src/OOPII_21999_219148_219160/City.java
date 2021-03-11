299489e7670b068512981131f4fce6adc63da54a
thanbimp <than.bimp@gmail.com>
2021-03-11 11:41:20 +0200

    added City class

---
+++ src/OOPII_21999_219148_219160/City.java
@@ -0,0 +1,27 @@
+package OOPII_21999_219148_219160;
+
+public class City {
+    private int[] terms_vector;
+    private float[] geodesic_vector;
+
+    public City(int[] terms_vector, float[] geodesic_vector) {
+        this.terms_vector = terms_vector;
+        this.geodesic_vector = geodesic_vector;
+    }
+
+    public int[] getTerms_vector() {
+        return terms_vector;
+    }
+
+    public void setTerms_vector(int[] terms_vector) {
+        this.terms_vector = terms_vector;
+    }
+
+    public float[] getGeodesic_vector() {
+        return geodesic_vector;
+    }
+
+    public void setGeodesic_vector(float[] geodesic_vector) {
+        this.geodesic_vector = geodesic_vector;
+    }
+}
--- src/OOPII_21999_219148_219160/Main.java
+++ src/OOPII_21999_219148_219160/Main.java
@@ -3,6 +3,5 @@ package OOPII_21999_219148_219160;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Lamarinas");
     }
 }
