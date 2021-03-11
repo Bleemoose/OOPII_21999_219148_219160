2cbac2de571c2745161d77c7480f1a0bed477137
thanbimp <than.bimp@gmail.com>
2021-03-11 15:28:37 +0200

    implemented location retrieval via OWM api

--- src/OOPII_21999_219148_219160/City.java
+++ src/OOPII_21999_219148_219160/City.java
@@ -1,13 +1,29 @@
 package OOPII_21999_219148_219160;
 
-public class City {
-    private int[] terms_vector;
-    private float[] geodesic_vector;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
-    public City(int[] terms_vector, float[] geodesic_vector) {
+import java.io.IOException;
+
+
+/*
+geodesic_vector[0] is longitude
+geodesic_vector[1] is latitude
+ */
+
+public class City {
+    public String name;
+    private int[] terms_vector=new int[10];
+    private float[] geodesic_vector=new float[2];
+
+    public City(String name) throws IOException {
         this.terms_vector = terms_vector;
         this.geodesic_vector = geodesic_vector;
+        geodesic_vector=getLocInfo(name);
     }
+
 
     public int[] getTerms_vector() {
         return terms_vector;
@@ -23,5 +39,21 @@ public class City {
 
     public void setGeodesic_vector(float[] geodesic_vector) {
         this.geodesic_vector = geodesic_vector;
+    }
+
+    private float[] getLocInfo(String  name) throws IOException {
+        float[] data=new float[2];
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=eeba41d7d3d95a8dad6d4c3ae375f602")
+                .method("GET", null)
+                .build();
+        String response = client.newCall(request).execute().body().string();
+        JsonObject jsonResponse = JsonParser.parseString(response).getAsJsonObject();
+        JsonObject jsonCoord=jsonResponse.getAsJsonObject("coord");
+        data[0]= Float.parseFloat(jsonCoord.get("lon").toString());
+        data[1]= Float.parseFloat(jsonCoord.get("lat").toString());
+        return data;
     }
 }
--- src/OOPII_21999_219148_219160/Main.java
+++ src/OOPII_21999_219148_219160/Main.java
@@ -1,5 +1,9 @@
 package OOPII_21999_219148_219160;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
