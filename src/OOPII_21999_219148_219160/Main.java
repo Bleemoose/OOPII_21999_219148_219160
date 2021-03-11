5b66de72698039b694567a83aa3abc8bc12edbfd
PolizoisT <61244111+PolizoisT@users.noreply.github.com>
2021-03-11 14:05:57 +0200

    testing git

--- src/OOPII_21999_219148_219160/Main.java
+++ src/OOPII_21999_219148_219160/Main.java
@@ -2,6 +2,7 @@ package OOPII_21999_219148_219160;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("impreza");
         System.out.println("Hello World!");
     }
 }
