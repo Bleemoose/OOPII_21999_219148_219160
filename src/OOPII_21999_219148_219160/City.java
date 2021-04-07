a743c8e273c432f5ab5e282e3c2aa147b2cb34c8
thanbimp <than.bimp@gmail.com>
2021-04-07 18:15:41 +0300

    cleaned up imports

--- src/OOPII_21999_219148_219160/City.java
+++ src/OOPII_21999_219148_219160/City.java
@@ -6,7 +6,6 @@ import com.google.gson.JsonParser;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import java.io.IOException;
-import java.io.InvalidClassException;
 
 
 /*
--- src/OOPII_21999_219148_219160/Main.java
+++ src/OOPII_21999_219148_219160/Main.java
@@ -1,6 +1,5 @@
 package OOPII_21999_219148_219160;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 /*
